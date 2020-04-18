92ee6d25b525f6e1dcd12dc5ed57aeed392ad820
Juveria Manzar <juveriadot@gmail.com>
2020-04-18 23:50:55 +0530

    create-heroes-2- routes-conrollers-views

--- mvc/controllers/index.js
+++ mvc/controllers/index.js
@@ -10,6 +10,22 @@ getHeroesForm = function(req, res) {
     res.render('create-a-hero', { title: 'Hall of Heroes' })
 }
 
+createNewHero = function({ body }, res) {
+    let hero = {
+        name: body.name,
+        desc: body.desc,
+        stats: {
+            strength: body.strength,
+            percepttion: body.percepttion,
+            endurance: body.endurance,
+            charisma: body.charisma,
+            intelligence: body.intelligence,
+            luck: body.luck
+        }
+    }
+    body.origin && (hero.origin = body.origin)
+}
+
 module.exports = {
     getIndex,
     getHeroesIndex,
--- mvc/routes/index.js
+++ mvc/routes/index.js
@@ -5,6 +5,7 @@ const ctrlIndex = require('../controllers/index');
 router.get('/', ctrlIndex.getIndex);
 router.get('/heroes', ctrlIndex.getHeroesIndex);
 router.get('/create-hero', ctrlIndex.getHeroesForm);
+router.post('/create-hero', ctrlIndex.createNewHero);
 
 
 module.exports = router;
