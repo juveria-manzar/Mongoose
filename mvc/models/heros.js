f2964dea16e340875583f66a4e3fff31eb2f009f
Juveria Manzar <juveriadot@gmail.com>
2020-04-18 22:51:18 +0530

    schema-2

--- mvc/models/heros.js
+++ mvc/models/heros.js
@@ -1,7 +1,20 @@
 const mongoose = require('mongoose')
 
+const statsSchema = new mongoose.Schema({
+    strength: { type: Number, default: 30, min: 0, max: 30 },
+    perception: { type: Number, default: 30, min: 0, max: 30 },
+    endurance: { type: Number, default: 30, min: 0, max: 30 },
+    charisma: { type: Number, default: 30, min: 0, max: 30 },
+    intelligence: { type: Number, default: 30, min: 0, max: 30 },
+    agility: { type: Number, default: 30, min: 0, max: 30 },
+    luck: { type: Number, default: 30, min: 0, max: 30 },
+})
+
 const heroSchema = new mongoose.Schema({
-    name: String
+    name: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
+    origin: { type: String, default: "unkown" },
+    stats: { type: statsSchema, required: true }
 })
 
 mongoose.model('Hero', heroSchema)
